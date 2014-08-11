9f23e1f11bb7dd6c8760e88db752d4687dc374be
Robert Court <robbie.court@gmail.com>
2014-08-11 20:59:05 +0100

    converted for function call

--- labelObjects.py
+++ labelObjects.py
@@ -3,25 +3,12 @@ import sys, os
 import nrrd
 from scipy import ndimage
 
-if (len(sys.argv) < 3):
-    print 'Error: missing arguments!'
-    print 'e.g. python labelObjects.py image.nrrd indexlabels.nrrd [intensity_threshold] [min_size] [connection_cube]'
-else:
-    print 'Loading image %s...'% (str(sys.argv[1]))
-    data1, header1 = nrrd.read(str(sys.argv[1]))
+def labelObj(imagefile, labelfile, ms=1000, t=20, sl=np.ones((3,3,3))):
+    print 'Loading image %s...'% (imagefile)
+    data1, header1 = nrrd.read(imagefile)
     header1.pop("endian", None)
-    t=20
-    ms=1000
-    sl = np.ones((3,3,3))
-    if (len(sys.argv) > 3):
-        t=np.uint16(sys.argv[3])
+
     print 'Labeling objects with any voxel intensity above %s'% str(t)
-
-    if (len(sys.argv) > 4):
-        ms=np.array(sys.argv[4],dtype=np.uint16)
-
-    if (len(sys.argv) > 5):
-        sl=np.array(sys.argv[5],dtype=np.uint16)
 
     data1[data1<t]=0
 
@@ -40,13 +27,34 @@ else:
         v=v+1
     print str(v-1) + ' distinct objects still indexed'
 
-    print "Saving result to " + str(sys.argv[2])
+    print "Saving result to " + labelfile
     header1['encoding'] = 'gzip'
     if v > 256:
       header1['type'] = 'uint16'
-      nrrd.write(str(sys.argv[2]), np.uint16(data), options=header1)
+      nrrd.write(labelfile, np.uint16(data), options=header1)
     else:
       header1['type'] = 'uint8'
-      nrrd.write(str(sys.argv[2]), np.uint8(data), options=header1)
+      nrrd.write(labelfile, np.uint8(data), options=header1)
 
-print 'done.'
+
+
+if __name__ == "__main__":
+  if (len(sys.argv) < 3):
+      print 'Error: missing arguments!'
+      print 'e.g. python labelObjects.py image.nrrd indexlabels.nrrd [intensity_threshold] [min_size] [connection_cube]'
+  else:
+      imagefile = str(sys.argv[1])
+      labelfile = str(sys.argv[2])
+
+      if (len(sys.argv) > 3):
+          t=np.uint16(sys.argv[3])
+
+      if (len(sys.argv) > 4):
+          ms=np.array(sys.argv[4],dtype=np.uint16)
+
+      if (len(sys.argv) > 5):
+          sl=np.array(sys.argv[5],dtype=np.uint16)
+
+      labelObj(imagefile, labelfile)
+
+  print 'done.'
