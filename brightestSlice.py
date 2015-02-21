6196d33afbf002fb53dfffbabdf42090163c8a5c
Robert Court <robbie.court@gmail.com>
2015-02-21 13:27:02 +0000

    finished script

--- brightestSlice.py
+++ brightestSlice.py
@@ -3,23 +3,17 @@ import sys, os
 import nrrd
 
 if (len(sys.argv) < 2):
-    print 'Error: missing arguments!'
-    print 'e.g. python brightestSlice.py image.nrrd'
+    print '0'
 else:
     Iin = str(sys.argv[1])
     print 'Processing %s...'% (Iin)
     data1, header1 = nrrd.read(Iin)
 
-    sh = np.shape(data1)
     print 'Image Size X:%s,Y:%s,Z:%s'% sh
 
     data1 = data1.sum(axis=0)
     data1 = data1.sum(axis=0)
 
-    sh = np.shape(data1)
-
     mp = np.argmax(data1)
 
-    print 'Number of slice %s'% (sh)
-    
-    print 'Max combined intensity occurs at slice %s'% (mp)
+    print '%s'% (mp)
