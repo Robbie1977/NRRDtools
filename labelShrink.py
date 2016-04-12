c67af4ab19ee1591906b39479f30231a6786312c
Rob Court <robbie.court@gmail.com>
2016-04-12 08:59:50 +0100

    Adding tool to remove index if template intensity below threshold

---
+++ labelShrink.py
@@ -0,0 +1,24 @@
+import png
+import numpy as np
+import sys, os
+import nrrd
+
+if (len(sys.argv) < 3):
+    print 'Error: missing arguments!' 
+    print 'e.g. python labelShrink.py template.nrrd index.nrrd minimumIntensity' 
+else:
+    print 'Loading image: %s...'% (str(sys.argv[1]))
+    data1, header1 = nrrd.read(str(sys.argv[1]))   
+    print 'Loading mask: %s...'% (str(sys.argv[2]))
+    data2, header2 = nrrd.read(str(sys.argv[2]))  
+    th = np.uint8(sys.argv[2])
+    if (data1.size <> data2.size):
+        print '\n\nError: Images must be the same size!!'
+    else:
+        print 'Removing any index for intensity values below %s...'% str(th)
+        data3=data2
+        data3[data1<th]=np.uint8(0)
+        print 'Saving result to %s...'% (str(sys.argv[3]))
+        nrrd.write(str(sys.argv[3]), np.uint8(data3), options=header1)    
+        print 'done.'
+        
