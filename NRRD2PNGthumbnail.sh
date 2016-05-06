ef1b9287fe43b01b587efcecd497c0c02819b5cd
Rob Court <robbie.court@gmail.com>
2016-05-06 10:52:46 +0100

    making headless an option

--- NRRD2PNGthumbnail.sh
+++ NRRD2PNGthumbnail.sh
@@ -3,22 +3,31 @@ echo 'export FIJI=path/fiji'
 echo 'export MACRO=ThisDir/createThumbnail.ijm'
 echo 'export TEMPLATE=path/VFB/t/001/background.nrrd'
 echo 'run in the directory above the volume.nrrd files'
+echo '-f forces thumbnail recreation'
+echo '-h runs in headless mode using xvfb-run'
 for file in ./*/volume.nrrd
 do
   if [ -f $file ]
   then
-    if [ -e ${file/volume.nrrd/thumbnail.png} ] && [ "$1" != "-f" ] && [ $MACRO -ot $ ${file/volume.nrrd/thumbnail.png} ] && [ ${MACRO/createThumbnail.ijm/NRRD2PNGthumbnail.sh} -ot $ ${file/volume.nrrd/thumbnail.png} ]
+    if [ -e ${file/volume.nrrd/thumbnail.png} ] && [[ $1 != *"f"* ]] && [ $MACRO -ot $ ${file/volume.nrrd/thumbnail.png} ] && [ ${MACRO/createThumbnail.ijm/NRRD2PNGthumbnail.sh} -ot $ ${file/volume.nrrd/thumbnail.png} ]
     then
       echo PNG file already exists! Skipping..
     else
       echo processing ${file}...
       # if forcing overwite then delete the old copy
-      if [ "$1" == "-f" ]
+      if [[ $1 == *"f"* ]]
       then
         rm ${file/volume.nrrd/thumbnail.png}
       fi
-      # generate thresholded surfaces using Fiji/ImageJ
-      xvfb-run $FIJI -macro $MACRO "$TEMPLATE,$file"
+      
+       # generate thumbnail using Fiji/ImageJ
+      if [[ $1 == *"h"* ]]
+      then
+        xvfb-run -w 10 $FIJI -macro $MACRO "$TEMPLATE,$file"
+      else
+        $FIJI -macro $MACRO "$TEMPLATE,$file"
+      fi
+      
     fi
   else
     echo Broken link for ${file}! Skipping..
