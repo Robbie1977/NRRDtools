7b77d08608ac5e44ca70a04144af33bf8a864a08
Rob Court <robbie.court@gmail.com>
2016-05-06 11:44:34 +0100

    removing timeout

--- NRRD2LayeredOBJ.sh
+++ NRRD2LayeredOBJ.sh
@@ -12,7 +12,7 @@ do
     then
       echo recent OBJ file already exists! Skipping..
     else
-      echo processing ${file}...
+      echo processing $(pwd)${file/.\//\/}...
       # if forcing overwite then delete the old copy
       if [ "$1" == "-f" ] 
       then
@@ -21,9 +21,9 @@ do
       # generate thresholded surfaces using Fiji/ImageJ
       if [[ $1 == *"h"* ]]
       then
-        xvfb-run -w 10 timeout 2m $FIJI -macro $MACRO $file
+        xvfb-run -w 10 $FIJI -macro $MACRO $file
       else
-        timeout 2m $FIJI -macro $MACRO $file
+        $FIJI -macro $MACRO $file
       fi
       # add basic file header
       echo '# Merge of thresholded surface wavefront .obj without sufaces at 1, 150 and 250 for VirtualFlyBrain.org.' > ${file/.nrrd/.tmp}
