1cd016fbb5d1440a3b74ae31ef94a530ed9a2a25
Robert Court <robbie.court@gmail.com>
2016-05-25 10:27:34 +0100

    tidying up

--- checkSWC.py
+++ checkSWC.py
@@ -9,17 +9,13 @@ lines = [line.strip() for line in open(str(sys.argv[1]))]
 for line in lines:
     if '#' not in line:
         values = line.split(' ')
-        xl = np.floor(np.float(values[2])) / np.float(header['space directions'][0][0])
-        yl = np.floor(np.float(values[3])) / np.float(header['space directions'][1][1])
-        zl = np.floor(np.float(values[4])) / np.float(header['space directions'][2][2])
-        xh = np.ceil(np.float(values[2])) / np.float(header['space directions'][0][0])
-        yh = np.ceil(np.float(values[3])) / np.float(header['space directions'][1][1])
-        zh = np.ceil(np.float(values[4])) / np.float(header['space directions'][2][2])
-        value = data[np.floor(xl):np.ceil(xh)+1, np.floor(yl):np.ceil(yh)+1, np.floor(zl):np.ceil(zh)+1]
+        xl = np.floor(np.floor(np.float(values[2])) / np.float(header['space directions'][0][0]))
+        yl = np.floor(np.floor(np.float(values[3])) / np.float(header['space directions'][1][1]))
+        zl = np.floor(np.floor(np.float(values[4])) / np.float(header['space directions'][2][2]))
+        xh = np.ceil(np.ceil(np.float(values[2])) / np.float(header['space directions'][0][0]))+1
+        yh = np.ceil(np.ceil(np.float(values[3])) / np.float(header['space directions'][1][1]))+1
+        zh = np.ceil(np.ceil(np.float(values[4])) / np.float(header['space directions'][2][2]))+1
+        value = data[xl:xh, yl:yh, zl:zh]
         if np.sum(value) < 1:
             print(line)
             print(value)
-            print(str(xl) + ',' + str(xh))
-            print(str(yl) + ',' + str(yh))
-            print(str(zl) + ',' + str(zh))
-            print(np.shape(data))
