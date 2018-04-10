17c3f252173c10d044089d2c55f3a960fff0ae9a
Rob Court <rcourt@ed.ac.uk>
2018-04-10 11:45:11 +0100

    Handling image errors

--- h5j2NRRD.sh
+++ h5j2NRRD.sh
@@ -25,10 +25,10 @@ do
       # convert n5j into nrrd
       if [[ $1 == *"h"* ]]
       then
-        xvfb-run -w 10 $FIJI -macro $MACRO $file
+        timeout 15m xvfb-run -w 10 $FIJI -macro $MACRO $file
         pkill Xvfb
       else
-        $FIJI -macro $MACRO $file
+        timeout 15m $FIJI -macro $MACRO $file
       fi
       sleep 5s
     fi
