145c2d7ca661b543e554bef51900ed82e5af5f8f
Rob Court <rcourt@ed.ac.uk>
2018-06-22 13:06:49 +0100

    making generic

--- image2NRRD.sh
+++ image2NRRD.sh
@@ -1,14 +1,15 @@
 echo 'before running specify the fiji executable and macro (located in this dir):'
 echo 'export FIJI=path/fiji'
-echo 'export MACRO=ThisDir/h5j2NRRD.ijm'
+echo 'export MACRO=ThisDir/image2NRRD.ijm'
+echo 'export EXT=h5j'
 echo 'run in the directory above the volume.nrrd files'
 echo '-f forces recreation'
 echo '-h runs in headless mode using xvfb-run'
-for file in $(pwd)/*/*.h5j
+for file in $(pwd)/*/*.${EXT}
 do
   echo $file
   if [ -f $file ]
-  output=${file/.h5j/.nrrd}
+  output=${file/${EXT}/.nrrd}
   output=$(echo $output|sed 's|\(.*\)/|\1/C1-|')
   echo $output
   then
