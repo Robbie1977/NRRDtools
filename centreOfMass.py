1ab1bc8cfe389eec28aeebd99dc45a7a0a6af6fa
Rob Court <rcourt@ed.ac.uk>
2018-10-18 13:45:48 +0100

    return list

---
+++ centreOfMass.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys, os
+import nrrd
+from scipy import ndimage
+
+if (len(sys.argv) < 2):
+    print('Error: missing arguments!')
+    print('e.g. python centreOfMass.py imageIn.nrrd')
+else:
+    Iin = str(sys.argv[1])
+    data1, header1 = nrrd.read(Iin)
+
+    print(list(ndimage.measurements.center_of_mass(Iin)))
+    
