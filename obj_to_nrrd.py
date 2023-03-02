c6da6ea5f041c6f1e940c3cd32329e647574c0b3
Rob Court <rcourt@ed.ac.uk>
2023-03-02 20:11:25 +0000

    fill_factor
     include a fill_factor argument that specifies the voxel size as a fraction of the minimum dimension of the bounding box of the mesh.

--- obj_to_nrrd.py
+++ obj_to_nrrd.py
@@ -1,43 +1,57 @@
-def obj_to_nrrd(input_file, output_file=None):
+def obj_to_nrrd(input_file, output_file=None, fill_factor=0.05):
     """
     Convert an OBJ file to a binary NRRD file with a physical size of 1 micron per voxel and microns as the unit for each axis.
 
     :param input_file: str, path to the input OBJ file
     :param output_file: str (optional), path to the output NRRD file. If not specified, the output file will have the same name as the input file but with the '.nrrd' extension
+    :param fill_factor: float (optional), voxel size as a fraction of the minimum dimension of the bounding box of the mesh. Default is 0.05 (1/20th of the minimum dimension).
     """
     import numpy as np
     import nrrd
     import os
+    import trimesh
 
     # Check if output file path is specified. If not, use the default output file name
     if output_file is None:
         output_file = os.path.splitext(input_file)[0] + '.nrrd'
 
-    # Load vertex data from OBJ file
+    # Load vertex data from OBJ file and create trimesh
     vertices = []
+    faces = []
     with open(input_file, 'r') as f:
         for line in f:
             if line.startswith('v '):
                 vertex = [float(x) for x in line.strip().split()[1:]]
                 vertices.append(vertex)
+            elif line.startswith('f '):
+                face = [int(x.split('/')[0])-1 for x in line.strip().split()[1:]]
+                faces.append(face)
+    trimesh_mesh = trimesh.Trimesh(vertices=vertices, faces=faces, process=False)
 
-    # Convert vertex data to binary NRRD file
-    max_coord = np.ceil(np.max(vertices, axis=0)).astype(int)
+    # Create a voxel grid that is large enough to fully enclose the trimesh
+    max_coord = np.ceil(trimesh_mesh.bounds[1]).astype(int)
     grid_shape = tuple(max_coord)
-    mesh = np.zeros(grid_shape, dtype=bool)  # create an empty binary mesh
-    scale_factor = np.ones(3, dtype=float)  # scale factor is 1 micron per voxel
-    scaled_vertices = np.round(vertices).astype(int) - 1  # subtract 1 to convert to 0-based indexing
-    mesh[tuple(scaled_vertices.T)] = True  # set binary value to True at each vertex coordinate
-    matrix = mesh.astype(np.uint8) * 255  # convert binary mesh to uint8 matrix
+    mesh = np.zeros(grid_shape, dtype=bool)
+
+    # Voxelized mesh and set binary values in mesh
+    voxel_size = np.min(max_coord) * fill_factor
+    volume = trimesh_mesh.voxelized(voxel_size).fill()
+    mesh[volume.points] = True
+
+    # Convert binary mesh to uint8 matrix
+    matrix = mesh.astype(np.uint8) * 255
+
+    # Set NRRD header with 1 micron scale factor and microns as the unit for each axis
     header = {
         'encoding': 'gzip',
         'space': 'right-anterior-superior',
         'space directions': [(1.0, 0, 0), (0, 1.0, 0), (0, 0, 1.0)],
         'space units': ['microns', 'microns', 'microns'],
         'kinds': ['domain', 'domain', 'domain']
-    }  # set NRRD header with 1 micron scale factor and microns as the unit for each axis
-    nrrd.write(output_file, matrix, header)  # save uint8 matrix as NRRD file using nrrd.write
+    }
 
+    # Save uint8 matrix as NRRD file using nrrd.write
+    nrrd.write(output_file, matrix, header)
 
 if __name__ == "__main__":
     import sys
