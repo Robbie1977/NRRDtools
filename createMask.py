a6401aa73ca8c3ccfa79dd1a764e25862153b070
Rob Court <rcourt@ed.ac.uk>
2023-03-03 17:03:50 +0000

    tidying up

--- createMask.py
+++ createMask.py
@@ -1,36 +1,51 @@
 import numpy as np
-import sys, os
+import sys
+import os
 import nrrd
 
+# Default threshold, number of positive voxels in the neighborhood, and the size of the neighborhood
 th = 35
 pt = 3
 bs = 5
 
-if (len(sys.argv) < 2):
-    print 'Error: missing arguments!'
-    print 'e.g. python createMask.py imageIn.nrrd [ImageOut.nrrd]'
-else:
-    Iin = str(sys.argv[1])
-    print 'Processing %s...'% (Iin)
-    data1, header1 = nrrd.read(Iin)
+def create_mask(input_file, output_file=None):
+    """
+    Create a binary mask from an input NRRD file using a threshold value.
 
-    sh = np.shape(data1)
+    :param input_file: str, path to the input NRRD file
+    :param output_file: str (optional), path to the output NRRD file. If not specified, the output file will have the same name as the input file but with the '-MD.nrrd' extension
+    """
+    if output_file is None:
+        output_file = input_file.replace(".nrrd", "-MD.nrrd")
 
-    if (len(sys.argv) > 2):
-      Iout = str(sys.argv[2])
+    # Load input data
+    data, header = nrrd.read(input_file)
+
+    # Create binary mask
+    mask = np.zeros_like(data, dtype=np.uint8)
+    mask[data > th] = np.uint8(255)
+
+    # Remove isolated voxels
+    # for x in range(bs, mask.shape[0] - bs):
+    #     for y in range(bs, mask.shape[1] - bs):
+    #         for z in range(bs, mask.shape[2] - bs):
+    #             if np.sum(mask[x-bs:x+bs, y-bs:y+bs, z-bs:z+bs]) < pt*255 and mask[x, y, z] > 0:
+    #                 mask[x, y, z] = 0
+    #                 sys.stdout.write('.')
+
+    # Save binary mask as NRRD file
+    nrrd.write(output_file, mask, options=header)
+
+    print(f"Saved binary mask to {output_file}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error: missing arguments!")
+        print("e.g. python createMask.py input.nrrd [output.nrrd]")
     else:
-      Iout = Iin.replace(".nrrd", "-MD.nrrd")
+        input_file = str(sys.argv[1])
+        output_file = None
+        if len(sys.argv) > 2:
+            output_file = str(sys.argv[2])
 
-
-    data = np.zeros(sh, dtype=np.uint8)
-    data[data1>th] = np.uint8(255)
-
-    # for x in range(5,sh[0]-6):
-    #   for y in range(5,sh[1]-6):
-    #     for z in range(5,sh[2]-6):
-    #       if ((np.sum(data[x-bs:x+bs,y-bs:y+bs,z-bs:z+bs]) < (pt*255)) and (data[x,y,z] > 0)):
-    #         data[x,y,z] = 0
-    #         sys.stdout.write('.')
-
-    nrrd.write(Iout, data, options=header1)
-    print 'saved to ' + Iout
+        create_mask(input_file, output_file=output_file)
