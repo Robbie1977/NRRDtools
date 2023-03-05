ad6206e5f005a26e87377dd91bff15d765671f28
Rob Court <rcourt@ed.ac.uk>
2023-03-05 16:07:12 +0000

    syntax fix

--- simple_obj_to_nrrd.py
+++ simple_obj_to_nrrd.py
@@ -51,7 +51,7 @@ def obj_to_nrrd(input_file, output_file=None, extent=None, radius=None, num_work
         chunks = np.array_split(vertices, num_workers)
         with Pool(num_workers) as pool:
             results = []
-            for i, result in enumerate(pool.imap_unordered(process_chunk, [(chunk, radius, grid_shape, scale_factor) for chunk in chunks])):
+            for i, result in enumerate(pool.imap_unordered(process_chunk, [(chunk, radius, grid_shape, scale_factor, progress_queue) for chunk in chunks])):
                 results.append(result)
                 print_progress(i + 1, len(chunks))
             mesh = np.zeros(grid_shape, dtype=bool)
