26f11bd41e7d03404e6dbe02c83ae06334cef34e
Rob Court <rcourt@ed.ac.uk>
2023-03-06 14:56:51 +0000

    Update rotate_image_stack.py

--- rotate_image_stack.py
+++ rotate_image_stack.py
@@ -6,16 +6,16 @@ def rotate_image_stack(data, voxel_size):
     axis_lengths = np.array(data.shape) * voxel_size
     longest_axis, next_axis = np.argsort(axis_lengths)[::-1][:2]
 
-    # Determine the number of rotations required to align the longest axis with the Y axis
-    num_rotations = (longest_axis - 2) % 3
+    # Determine the number of rotations required to align the longest axis with the X axis
+    num_rotations = (longest_axis - 1) % 3
 
     # Rotate the data and the voxel size accordingly
     for _ in range(num_rotations):
         data = np.rot90(data, axes=(2, 1))
         voxel_size = np.roll(voxel_size, 1)
 
-    # Determine the axis permutation required to put the next longest axis in the X axis
-    axis_permutation = [1, 2, 0] if next_axis == 0 else [0, 2, 1]
+    # Determine the axis permutation required to put the next longest axis in the Y axis
+    axis_permutation = [2, 0, 1] if next_axis == 0 else [1, 0, 2]
 
     # Permute the axes of the data and the voxel size accordingly
     data = np.transpose(data, axis_permutation)
