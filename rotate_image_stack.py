3387ffe01166a94bf12aa97c62497235c8559fb7
Rob Court <rcourt@ed.ac.uk>
2023-03-06 14:49:49 +0000

    longest on width

--- rotate_image_stack.py
+++ rotate_image_stack.py
@@ -6,16 +6,16 @@ def rotate_image_stack(data, voxel_size):
     axis_lengths = np.array(data.shape) * voxel_size
     longest_axis, next_axis = np.argsort(axis_lengths)[::-1][:2]
 
-    # Determine the number of rotations required to align the longest axis with the X axis
-    num_rotations = (longest_axis - 1) % 3
+    # Determine the number of rotations required to align the longest axis with the Y axis
+    num_rotations = (longest_axis - 2) % 3
 
     # Rotate the data and the voxel size accordingly
     for _ in range(num_rotations):
-        data = np.rot90(data, axes=(1, 0))
+        data = np.rot90(data, axes=(2, 1))
         voxel_size = np.roll(voxel_size, 1)
 
-    # Determine the axis permutation required to put the longest axis in the X axis and next longest axis in the Y axis
-    axis_permutation = [0, 1, 2] if longest_axis == 0 else [1, 0, 2]
+    # Determine the axis permutation required to put the next longest axis in the X axis
+    axis_permutation = [2, 1, 0] if next_axis == 0 else [1, 0, 2]
 
     # Permute the axes of the data and the voxel size accordingly
     data = np.transpose(data, axis_permutation)
