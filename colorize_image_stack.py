78c452f31ab570330fa809db3a2bb7bd69e335ec
Rob Court <rcourt@ed.ac.uk>
2023-03-08 11:54:43 +0000

    Update colorize_image_stack.py

--- colorize_image_stack.py
+++ colorize_image_stack.py
@@ -68,12 +68,10 @@ def colorize_image_stack(nrrd_path, png_path, thumbnail=False, add_scale=True):
     if add_scale:
         print('Add color bar to the right of the image')
         # Add color bar to the right side of the image
-        print(voxel_size)
-        print(height)
         color_bar_width = 2
         color_bar_height = height
         color_bar = np.zeros((color_bar_height, color_bar_width, 3), dtype=np.uint8)
-        bar_ratio = voxel_size[0] / voxel_size[2]
+        bar_ratio = height / depth
         print(bar_ratio)
         for y in range(color_bar_height):
             index = y * bar_ratio
