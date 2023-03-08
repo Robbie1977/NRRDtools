344f8a181366cd837f55c3c4e6b2904b4ca0e992
Rob Court <rcourt@ed.ac.uk>
2023-03-08 09:55:23 +0000

    Create make_transparent.py

---
+++ make_transparent.py
@@ -0,0 +1,28 @@
+import argparse
+from PIL import Image
+
+def make_transparent(input_file, output_file):
+    with Image.open(input_file) as im:
+        # Convert the image to RGBA mode
+        im = im.convert("RGBA")
+        # Get the color of the top-left pixel
+        bg_color = im.getpixel((0, 0))
+        # Make all pixels with the same color as the top-left pixel transparent
+        im = im.convert("RGBA")
+        data = im.getdata()
+        new_data = []
+        for item in data:
+            if item[:3] == bg_color:
+                new_data.append((255, 255, 255, 0))
+            else:
+                new_data.append(item)
+        im.putdata(new_data)
+        # Save the transparent image
+        im.save(output_file)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='Input PNG file')
+    parser.add_argument('output', help='Output PNG file')
+    args = parser.parse_args()
+    make_transparent(args.input, args.output)
