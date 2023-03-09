2778a0333db6a58da98bad58e6901a9aa34bba95
Rob Court <rcourt@ed.ac.uk>
2023-03-09 16:04:51 +0000

    updated for python3

--- labelObjects.py
+++ labelObjects.py
@@ -7,30 +7,30 @@ import nrrd
 
 
 def labelObj(imagefile, labelfile, t=20, ms=1000, sl=np.ones((3, 3, 3))):
-    print 'Loading image %s...' % (imagefile)
+    print('Loading image %s...' % (imagefile))
     data1, header1 = nrrd.read(imagefile)
     header1.pop("endian", None)
 
-    print 'Labeling objects with any voxel intensity above %s' % str(t)
+    print('Labeling objects with any voxel intensity above %s' % str(t))
 
     data1[data1 < t] = 0
 
-    print 'identifying distinct objects...'
+    print('identifying distinct objects...')
     labels, features = ndimage.label(data1, structure=sl)
-    print str(features) + ' distinct objects found'
+    print(str(features) + ' distinct objects found')
     val, lab = np.histogram(labels, bins=range(1, features + 1))
-    print 'Removing any objects with a volume of less than ' + str(ms) + ' voxels.'
-    print 'New label(s):'
+    print('Removing any objects with a volume of less than ' + str(ms) + ' voxels.')
+    print('New label(s):')
     data = np.zeros(np.shape(data1))
     v = 1
     for i in range(0, features - 1):
         if val[i] > ms:
             data[labels == lab[i]] = v
-            print str(v) + ' = ' + str(lab[i])
+            print(str(v) + ' = ' + str(lab[i]))
             v = v + 1
-    print str(v - 1) + ' distinct objects still indexed'
+    print(str(v - 1) + ' distinct objects still indexed')
 
-    print "Saving result to " + labelfile
+    print("Saving result to " + labelfile)
     header1['encoding'] = 'gzip'
     if v > 256:
         header1['type'] = 'uint16'
@@ -43,24 +43,24 @@ def labelObj(imagefile, labelfile, t=20, ms=1000, sl=np.ones((3, 3, 3))):
 
 def cutObj(imagefile, labelfile, labels=None):
     if labels is None:
-        print 'no labels to be cut'
+        print('no labels to be cut')
     else:
-        print 'Loading image %s...' % (imagefile)
+        print('Loading image %s...' % (imagefile))
         data1, header1 = nrrd.read(imagefile)
-        print 'Loading mask %s...' % (labelfile)
+        print('Loading mask %s...' % (labelfile))
         data2, header2 = nrrd.read(labelfile)
 
-        print 'Cutting objects with label(s) %s' % str(labels)
+        print('Cutting objects with label(s) %s' % str(labels))
 
         for i in labels:
             try:
-                print 'Cutting object ' + str(i)
+                print('Cutting object ' + str(i))
                 data1[data2 == int(i)] = np.uint8(0)
             except:
-                print '---'
+                print('---')
 
         v = np.max(data1)
-        print "Saving result over " + imagefile
+        print("Saving result over " + imagefile)
         header1['encoding'] = 'gzip'
         if v > 256:
             header1['type'] = 'uint16'
@@ -72,27 +72,27 @@ def cutObj(imagefile, labelfile, labels=None):
 
 def cropObj(imagefile, labelfile, labels=None):
     if labels is None:
-        print 'no labels to crop to'
+        print('no labels to crop to')
     else:
-        print 'Loading image %s...' % (imagefile)
+        print('Loading image %s...' % (imagefile))
         data1, header1 = nrrd.read(imagefile)
-        print 'Loading mask %s...' % (labelfile)
+        print('Loading mask %s...' % (labelfile))
         data2, header2 = nrrd.read(labelfile)
 
-        print 'Croping to objects with label(s) %s' % str(labels)
+        print('Croping to objects with label(s) %s' % str(labels))
 
         mask = np.ones(np.shape(data1), dtype=bool)
         for i in labels:
             try:
-                print 'Cropping to object ' + str(i)
+                print('Cropping to object ' + str(i))
                 mask[data2 == int(i)] = False
             except:
-                print '---'
+                print('---')
 
         data1[mask] = 0
 
         v = np.max(data1)
-        print "Saving result over " + imagefile
+        print("Saving result over " + imagefile)
         header1['encoding'] = 'gzip'
         if v > 256:
             header1['type'] = 'uint16'
@@ -104,8 +104,8 @@ def cropObj(imagefile, labelfile, labels=None):
 
 if __name__ == "__main__":
     if (len(sys.argv) < 3):
-        print 'Error: missing arguments!'
-        print 'e.g. python labelObjects.py image.nrrd indexlabels.nrrd [intensity_threshold] [min_size] [connection_cube]'
+        print('Error: missing arguments!')
+        print('e.g. python labelObjects.py image.nrrd indexlabels.nrrd [intensity_threshold] [min_size] [connection_cube]')
     else:
         imagefile = str(sys.argv[1])
         labelfile = str(sys.argv[2])
@@ -124,4 +124,4 @@ if __name__ == "__main__":
 
         labelObj(imagefile, labelfile, t=t, ms=ms, sl=sl)
 
-    print 'done.'
+    print('done.')
