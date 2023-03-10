8f25573efd63a6f1c5ce5ebc0770a1ab2fa89fec
Rob Court <rcourt@ed.ac.uk>
2023-03-10 07:11:05 +0000

    adding template caching

--- create_thumbnail.py
+++ create_thumbnail.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import random
 
-def create_thumbnail(template_file, signal_file, output_file):
+def create_thumbnail(template_file, signal_file, output_file, cache_template=False):
     # Set up paths to the necessary files
     merge_images_path = os.path.join(os.getcwd(), "merge_images.py")
     colorize_image_stack_path = os.path.join(os.getcwd(), "colorize_image_stack.py")
@@ -11,17 +11,26 @@ def create_thumbnail(template_file, signal_file, output_file):
     # Generate a random number for temporary file names
     rand_num = str(random.randint(1000, 9999))
     
+    if cache_template:
+        # extract the template id from the VFB specific folder structure
+        template_id = parent_dir = os.path.basename(os.path.dirname(
+        template_mip = f"template_mip_{template_id}.png"
+    else:
+        template_mip = "template_mip_{rand_num}.png"
+   
     # Create the maximum intensity projection of the template
-    os.system(f"python3 {create_mip_path} {template_file} template_mip_{rand_num}.png")
+    if not cache_template or not os.path.exists(template_mip): 
+        os.system(f"python3 {create_mip_path} {template_file} {template_mip}")
     
     # Colorize the signal image
     os.system(f"python3 {colorize_image_stack_path} --nrrd {signal_file} --png signal_colorized_{rand_num}.png --scale")
     
     # Merge the template and colorized signal images
-    os.system(f"python3 {merge_images_path} template_mip_{rand_num}.png signal_colorized_{rand_num}.png {output_file}")
+    os.system(f"python3 {merge_images_path} {template_mip} signal_colorized_{rand_num}.png {output_file}")
     
     # Clean up temporary files
-    os.remove(f"template_mip_{rand_num}.png")
+    if not cache_template:
+        os.remove(f"{template_mip}")
     os.remove(f"signal_colorized_{rand_num}.png")
 
 def main():
@@ -32,12 +41,13 @@ def main():
     parser.add_argument("template_file", help="Path to Template.nrrd file")
     parser.add_argument("signal_file", help="Path to Signal.nrrd file")
     parser.add_argument("output_file", help="Path to output thumbnail.png file")
+    parser.add_argument('--cache', action='store_true', help='If added then the template mips are cached')
     
     # Parse arguments
     args = parser.parse_args()
     
     # Create thumbnail
-    create_thumbnail(args.template_file, args.signal_file, args.output_file)
+    create_thumbnail(args.template_file, args.signal_file, args.output_file, args.cache)
 
 if __name__ == "__main__":
     main()
