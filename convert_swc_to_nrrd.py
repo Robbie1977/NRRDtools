62d0a050130a6ff27de1be577fe252e555160d15
Rob Court <rcourt@ed.ac.uk>
2023-03-10 12:15:42 +0000

    Update convert_swc_to_nrrd.py

--- convert_swc_to_nrrd.py
+++ convert_swc_to_nrrd.py
@@ -4,52 +4,56 @@ import argparse
 from math import pi
 
 def convert_swc_to_nrrd(swc_file, template_file, output_file):
-    # Load the SWC data
+    # Load SWC file
     swc_data = np.loadtxt(swc_file, comments='#')
-
-    # Load the template image and extract the voxel dimensions
-    template, options = nrrd.read(template_file)
-    voxel_size = options['spacings']
-
-    # Calculate the dimensions of the output volume
-    dims = template.shape
-
-    # Create an empty volume
-    volume = np.zeros(dims, dtype=np.uint8)
-
-    # Loop through the SWC data and draw lines between each point and its parent
-    for i in range(len(swc_data)):
-        # Extract the X, Y, and Z coordinates
-        x = int(swc_data[i, 2] / voxel_size[0])
-        y = int(swc_data[i, 3] / voxel_size[1])
-        z = int(swc_data[i, 4] / voxel_size[2])
-
-        # If this is the soma, draw it as a sphere with the given radius
-        if swc_data[i, 1] == 1:
-            radius = int(swc_data[i, 5] / voxel_size[0])
-            for xi in range(max(0, x-radius), min(dims[0], x+radius)):
-                for yi in range(max(0, y-radius), min(dims[1], y+radius)):
-                    for zi in range(max(0, z-radius), min(dims[2], z+radius)):
-                        if (xi-x)**2 + (yi-y)**2 + (zi-z)**2 <= radius**2:
-                            volume[xi, yi, zi] = 255
-        # Otherwise, draw a line to the parent
+    
+    # Load template NRRD file
+    data, header = nrrd.read(template_file)
+    # Get the dimensions of the volume
+    dims = header['sizes']
+    # Get the direction vectors for each axis
+    directions = header['space directions']
+    # Get the origin of the volume
+    origin = header['space origin']
+    
+    # Create a new volume for the SWC data
+    volume = np.zeros(dims)
+    
+    # Draw each point in the SWC file as a sphere
+    for i in range(swc_data.shape[0]):
+        point = swc_data[i, 2:5].astype(int)
+        point -= origin.astype(int)
+        radius = swc_data[i, 5]
+        label = swc_data[i, 1]
+        
+        # If the point is a soma, draw it as a sphere
+        if label == 1:
+            # Get the coordinates of each voxel within the sphere
+            x_range = np.arange(-radius, radius+1)
+            y_range = np.arange(-radius, radius+1)
+            z_range = np.arange(-radius, radius+1)
+            xv, yv, zv = np.meshgrid(x_range, y_range, z_range, indexing='ij')
+            coords = np.array([xv.flatten(), yv.flatten(), zv.flatten()]).T
+            dist = np.linalg.norm(coords, axis=1)
+            sphere_mask = dist <= radius
+            
+            # Convert sphere coordinates to volume coordinates
+            sphere_coords = (coords[sphere_mask] + point).T
+            
+            # Set the value of the volume at the sphere coordinates to 255
+            volume[sphere_coords[0], sphere_coords[1], sphere_coords[2]] = 255
+        # Otherwise, draw the point as a single voxel
         else:
-            parent = int(swc_data[i, 6])
-            if parent >= 0:
-                parent_x = int(swc_data[parent-1, 2] / voxel_size[0])
-                parent_y = int(swc_data[parent-1, 3] / voxel_size[1])
-                parent_z = int(swc_data[parent-1, 4] / voxel_size[2])
-                direction = [x - parent_x, y - parent_y, z - parent_z]
-                distance = np.sqrt(direction[0]**2 + direction[1]**2 + direction[2]**2)
-                direction = [d / distance for d in direction]
-                for j in range(int(distance)):
-                    xi = int(parent_x + j * direction[0])
-                    yi = int(parent_y + j * direction[1])
-                    zi = int(parent_z + j * direction[2])
-                    volume[xi, yi, zi] = 255
-
-    # Save the output volume as an NRRD file
-    nrrd.write(output_file, volume, options=options)
+            # Convert point coordinates to volume coordinates
+            point -= 1  # SWC format starts indexing at 1
+            point = np.dot(directions, point)
+            point = point.astype(int)
+            
+            # Set the value of the volume at the point coordinates to 255
+            volume[point[0], point[1], point[2]] = 255
+    
+    # Write the output NRRD file
+    nrrd.write(output_file, volume, header=header)
 
 def main():
     # Create argument parser
