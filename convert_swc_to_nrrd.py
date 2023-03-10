efa78aed27243f8814e4503d36b9047e66720a2f
Rob Court <rcourt@ed.ac.uk>
2023-03-10 11:55:47 +0000

    refactoring for numpy

--- convert_swc_to_nrrd.py
+++ convert_swc_to_nrrd.py
@@ -1,55 +1,44 @@
 import numpy as np
 import nrrd
 
+import numpy as np
+import nrrd
+
 def convert_swc_to_nrrd(swc_file, template_file, output_file):
-    # Load the SWC file
-    with open(swc_file) as f:
-        swc_lines = f.readlines()
-        
-    # Extract the coordinates and radii from the SWC file
-    coords = []
-    radii = []
-    for line in swc_lines:
-        if line.startswith('#'):
-            continue
-        data = line.strip().split()
-        coords.append([float(data[2]), float(data[3]), float(data[4])])
-        radii.append(float(data[5]))
+    # Load the template nrrd file
+    data, header = nrrd.read(template_file)
+    voxel_size = header['space directions'][0][0]
 
-    # Load the template file to get the voxel size and dimensions
-    template_data, template_header = nrrd.read(template_file)
-    voxel_size = template_header['space directions'][0][0]
-    dimensions = template_data.shape
+    # Load the swc file
+    swc_data = np.loadtxt(swc_file, dtype=np.float32, comments='#', usecols=(2, 3, 4, 5, 6), ndmin=2)
 
-    # Create the output NRRD file with the same dimensions and voxel size as the template
-    output_data = np.zeros(dimensions, dtype=np.uint8)
-    output_header = {
-        'type': 'uint8',
-        'dimension': 3,
-        'space': 'left-posterior-superior',
-        'sizes': dimensions,
-        'space directions': np.array([[voxel_size,0,0],[0,voxel_size,0],[0,0,voxel_size]]),
-        'encoding': 'gzip'
-    }
+    # Create an empty volume
+    volume_shape = data.shape
+    volume = np.zeros(volume_shape, dtype=np.uint8)
 
-    # Draw each line segment in the SWC file
-    for i in range(1, len(coords)):
-        parent = coords[i-1]
-        child = coords[i]
-        parent_radius = radii[i-1] / 2
-        child_radius = radii[i] / 2
-        direction = child - parent
-        distance = np.linalg.norm(direction)
-        direction = direction / distance
-        num_steps = int(np.ceil(distance / voxel_size))
-        for j in range(num_steps):
-            step = parent + (j / num_steps) * direction * distance
-            output_data[int(round(step[0]/voxel_size)), int(round(step[1]/voxel_size)), int(round(step[2]/voxel_size))] = child_radius
-            for k in range(1, int(round(parent_radius/voxel_size))):
-                output_data[int(round(parent[0]/voxel_size)), int(round(parent[1]/voxel_size)), int(round(parent[2]/voxel_size))] = 255 - (255 * k / int(round(parent_radius/voxel_size)))
+    # Fill in the volume with the swc data
+    for i in range(swc_data.shape[0]):
+        x, y, z, radius, parent = swc_data[i]
+        x = int(round(x / voxel_size))
+        y = int(round(y / voxel_size))
+        z = int(round(z / voxel_size))
+        radius = int(round(radius / voxel_size))
+        if i > 0:
+            parent = int(parent) - 1
+            child = np.array([x, y, z])
+            parent_point = swc_data[parent, :3]
+            parent_point = np.array(parent_point)
+            direction = child - parent_point
+            distance = np.linalg.norm(direction)
+            direction = direction / distance
+            for j in range(1, int(distance)):
+                point = parent_point + j * direction
+                point = np.round(point).astype(int)
+                volume[point[0], point[1], point[2]] = 255
+        volume[x, y, z] = 255
 
-    # Write the output NRRD file
-    nrrd.write(output_file, output_data, output_header)
+    # Save the output file
+    nrrd.write(output_file, volume, header=header)
 
 if __name__ == "__main__":
     import argparse
