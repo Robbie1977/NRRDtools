db63863290b4353d807bd21432d26aabe1c77f43
Rob Court <rcourt@ed.ac.uk>
2023-03-10 12:06:19 +0000

    drawing soma

--- convert_swc_to_nrrd.py
+++ convert_swc_to_nrrd.py
@@ -1,50 +1,67 @@
 import numpy as np
 import nrrd
-
-import numpy as np
-import nrrd
+import argparse
+from math import pi
 
 def convert_swc_to_nrrd(swc_file, template_file, output_file):
-    # Load the template nrrd file
-    data, header = nrrd.read(template_file)
-    voxel_size = header['space directions'][0][0]
+    # Load the SWC data
+    swc_data = np.loadtxt(swc_file, comments='#')
 
-    # Load the swc file
-    swc_data = np.loadtxt(swc_file, dtype=np.float32, comments='#', usecols=(2, 3, 4, 5, 6), ndmin=2)
+    # Load the template image and extract the voxel dimensions
+    template, options = nrrd.read(template_file)
+    voxel_size = options['spacings']
+
+    # Calculate the dimensions of the output volume
+    dims = template.shape
 
     # Create an empty volume
-    volume_shape = data.shape
-    volume = np.zeros(volume_shape, dtype=np.uint8)
+    volume = np.zeros(dims, dtype=np.uint8)
 
-    # Fill in the volume with the swc data
-    for i in range(swc_data.shape[0]):
-        x, y, z, radius, parent = swc_data[i]
-        x = int(round(x / voxel_size))
-        y = int(round(y / voxel_size))
-        z = int(round(z / voxel_size))
-        radius = int(round(radius / voxel_size))
-        if i > 0:
-            parent = int(parent) - 1
-            child = np.array([x, y, z])
-            parent_point = swc_data[parent, :3]
-            parent_point = np.array(parent_point)
-            direction = child - parent_point
-            distance = np.linalg.norm(direction)
-            direction = direction / distance
-            for j in range(1, int(distance)):
-                point = parent_point + j * direction
-                point = np.round(point).astype(int)
-                volume[point[0], point[1], point[2]] = 255
-        volume[x, y, z] = 255
+    # Loop through the SWC data and draw lines between each point and its parent
+    for i in range(len(swc_data)):
+        # Extract the X, Y, and Z coordinates
+        x = int(swc_data[i, 2] / voxel_size[0])
+        y = int(swc_data[i, 3] / voxel_size[1])
+        z = int(swc_data[i, 4] / voxel_size[2])
 
-    # Save the output file
-    nrrd.write(output_file, volume, header=header)
+        # If this is the soma, draw it as a sphere with the given radius
+        if swc_data[i, 1] == 1:
+            radius = int(swc_data[i, 5] / voxel_size[0])
+            for xi in range(max(0, x-radius), min(dims[0], x+radius)):
+                for yi in range(max(0, y-radius), min(dims[1], y+radius)):
+                    for zi in range(max(0, z-radius), min(dims[2], z+radius)):
+                        if (xi-x)**2 + (yi-y)**2 + (zi-z)**2 <= radius**2:
+                            volume[xi, yi, zi] = 255
+        # Otherwise, draw a line to the parent
+        else:
+            parent = int(swc_data[i, 6])
+            if parent >= 0:
+                parent_x = int(swc_data[parent-1, 2] / voxel_size[0])
+                parent_y = int(swc_data[parent-1, 3] / voxel_size[1])
+                parent_z = int(swc_data[parent-1, 4] / voxel_size[2])
+                direction = [x - parent_x, y - parent_y, z - parent_z]
+                distance = np.sqrt(direction[0]**2 + direction[1]**2 + direction[2]**2)
+                direction = [d / distance for d in direction]
+                for j in range(int(distance)):
+                    xi = int(parent_x + j * direction[0])
+                    yi = int(parent_y + j * direction[1])
+                    zi = int(parent_z + j * direction[2])
+                    volume[xi, yi, zi] = 255
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="Convert a SWC file to an NRRD file")
-    parser.add_argument("swc_file", help="Path to SWC file")
-    parser.add_argument("template_file", help="Path to template NRRD file")
+    # Save the output volume as an NRRD file
+    nrrd.write(output_file, volume, options=options)
+
+def main():
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Convert SWC file to NRRD file")
+    
+    # Add arguments
+    parser.add_argument("swc_file", help="Path to input SWC file")
+    parser.add_argument("template_file", help="Path to input template NRRD file")
     parser.add_argument("output_file", help="Path to output NRRD file")
+    
+    # Parse arguments
     args = parser.parse_args()
-    convert_swc_to_nrrd(args.swc_file, args.template_file, args.output_file)
+    
+    # Convert SWC to NRRD
+    convert_swc_to_nrrd(args
