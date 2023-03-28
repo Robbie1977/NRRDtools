daf7650deeaa49253e32d9cfb7864def84618d47
Rob Court <rcourt@ed.ac.uk>
2023-03-28 18:31:29 +0100

    syntax fix

--- swc_to_nrrd.py
+++ swc_to_nrrd.py
@@ -41,7 +41,7 @@ def create_volume_from_swc(swc_data, dims, voxel_size, minRadius=0.005):
 
 def convert_swc_to_nrrd(swc_file, template_file, output_file):
     swc_data = read_swc(swc_file)
-    max_coords = np.max(swc_data[['x', 'y', 'z']], axis=0)
+    max_coords = np.max(np.array([swc_data['x'], swc_data['y'], swc_data['z']]), axis=1)
     print(f"Max SWC coordinates: {max_coords}")
 
     nrrd_template, options = nrrd.read(template_file)
