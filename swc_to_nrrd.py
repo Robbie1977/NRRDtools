cfbcad92ce127d2198078a81001a689f77a6bee1
Rob Court <rcourt@ed.ac.uk>
2023-03-28 18:16:02 +0100

    refactoring scaling

--- swc_to_nrrd.py
+++ swc_to_nrrd.py
@@ -47,9 +47,9 @@ def scale_volume(volume, scale_factors):
 
     x, y, z = np.meshgrid(np.arange(output_shape[0]), np.arange(output_shape[1]), np.arange(output_shape[2]), indexing='ij')
     
-    x_indices = np.minimum((x * scale_factors[0]).astype(int), input_shape[0] - 1)
-    y_indices = np.minimum((y * scale_factors[1]).astype(int), input_shape[1] - 1)
-    z_indices = np.minimum((z * scale_factors[2]).astype(int), input_shape[2] - 1)
+    x_indices = np.minimum((x / scale_factors[0]).astype(int), input_shape[0] - 1)
+    y_indices = np.minimum((y / scale_factors[1]).astype(int), input_shape[1] - 1)
+    z_indices = np.minimum((z / scale_factors[2]).astype(int), input_shape[2] - 1)
     
     output_volume = np.zeros(output_shape, dtype=volume.dtype)
     output_volume[x, y, z] = volume[x_indices, y_indices, z_indices]
@@ -60,22 +60,19 @@ def convert_swc_to_nrrd(swc_file, template_file, output_file):
     swc_data = read_swc(swc_file)
     nrrd_template, options = nrrd.read(template_file)
     space_directions = options['space directions']
-    input_voxel_size = np.array([1.0, 1.0, 1.0])  # 1x1x1 um voxel size
-    target_voxel_size = [np.linalg.norm(direction) for direction in space_directions]
-    scale_factors = np.divide(target_voxel_size, input_voxel_size)
-    dims = np.ceil(np.divide(np.shape(nrrd_template),target_voxel_size)).astype(int)
+    voxel_size = [np.linalg.norm(direction) for direction in space_directions]
+    dims = np.ceil(np.divide(np.shape(nrrd_template),voxel_size)).astype(int)
 
     print(f"micron space image shape: {dims}")
     
     volume = create_volume_from_swc(swc_data, dims)
-    volume = scale_volume(volume, scale_factors)
+    volume = scale_volume(volume, np.array(voxel_size) / np.array([1, 1, 1]))
 
     print(f"Output image shape: {np.shape(volume)}")
     
     nrrd.write(output_file, volume.astype(np.uint8), header=options)
     
     print(f"Saved NRRD file: {output_file}")
-
 
 if __name__ == "__main__":
     # Create argument parser
