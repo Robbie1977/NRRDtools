fb9531edb5d4f5519cd11cfff69570fcdd3b4cf8
Rob Court <rcourt@ed.ac.uk>
2023-03-28 16:02:13 +0100

    scaling up space for volume

--- swc_to_nrrd.py
+++ swc_to_nrrd.py
@@ -44,7 +44,7 @@ def convert_swc_to_nrrd(swc_file, template_file, output_file):
     nrrd_template, options = nrrd.read(template_file)
     space_directions = options['space directions']
     voxel_size = [np.linalg.norm(direction) for direction in space_directions]
-    dims = nrrd_template.shape
+    dims = np.ceil(np.divide(nrrd_template.shape,voxel_size))
 
     volume = create_volume_from_swc(swc_data, dims)
     volume = np.repeat(volume, voxel_size[0], axis=0)
